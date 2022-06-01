b8ee784d3f234771e86059d4836a055e77fa9b86
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-01 21:04:16 +0700

    Change the minimum password length

--- model/payload/login.go
+++ model/payload/login.go
@@ -2,5 +2,5 @@ package payload
 
 type Login struct {
 	Username string `json:"username" validate:"nonzero,min=2,max=20" extensions:"x-order=0"`
-	Password string `json:"password" validate:"nonzero,min=5,max=20" extensions:"x-order=1"`
+	Password string `json:"password" validate:"nonzero,min=8,max=20" extensions:"x-order=1"`
 }
--- model/payload/register.go
+++ model/payload/register.go
@@ -4,5 +4,5 @@ type Register struct {
 	Username string `json:"username" validate:"nonzero,min=2,max=20" extensions:"x-order=0"`
 	Email    string `json:"email" validate:"nonzero,min=5,max=50" extensions:"x-order=1"`
 	Name     string `json:"name" validate:"nonzero,min=1,max=50" extensions:"x-order=2"`
-	Password string `json:"password" validate:"nonzero,min=5,max=20" extensions:"x-order=3"`
+	Password string `json:"password" validate:"nonzero,min=8,max=20" extensions:"x-order=3"`
 }
