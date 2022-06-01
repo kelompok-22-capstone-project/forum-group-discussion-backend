d08124fb6e1314c1e4520d97348c6fcb5c0b2ce7
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-01 19:31:19 +0700

    Initialize the PostgreSQL database

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/config"
 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/controller"
 	_ "github.com/kelompok-22-capstone-project/forum-group-discussion-backend/docs"
 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/middleware"
@@ -34,6 +35,13 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	if err := godotenv.Load(".env"); err != nil {
 		log.Printf("Error loading .env file: %s\n", err.Error())
+	}
+
+	db, err := config.NewPostgreSQLDatabase()
+	if err != nil {
+		log.Fatalln(err.Error())
+	} else {
+		log.Printf("Successfully connected to database with instance address: %p", db)
 	}
 
 	port := ":" + os.Getenv("PORT")
