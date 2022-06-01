9804e3d7d8e678b9144e157c30d91c01ca5a7321
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-01 11:15:25 +0700

    Create the database migration file to insert initial admin data

---
+++ migrations/20220601041120_insert_initial_admin.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (id, username, email, name, password, role)
+VALUES ('u-Er5Spz',
+        'admin',
+        'admin22@gmail.com',
+        'Admin Kelompok 22',
+        '$2a$10$xeH/BjHaU3Mcs1GqpGGfa.MHuy6Vcmm9ukdPHDHBWo8SI.99KBzSK',
+        'admin');
