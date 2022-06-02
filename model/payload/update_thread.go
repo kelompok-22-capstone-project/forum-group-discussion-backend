5f2b5e329af516798d360c2b0adbf6d84a302996
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-02 18:25:58 +0700

    Create update_thread.go

---
+++ model/payload/update_thread.go
@@ -0,0 +1,7 @@
+package payload
+
+type UpdateThread struct {
+	Title       string `json:"title" validate:"nonzero,min=2,max=50" extensions:"x-order=0"`
+	Description string `json:"description" validate:"nonzero,min=2" extensions:"x-order=1"`
+	CategoryID  string `json:"categoryID" validate:"nonzero,min=4,max=6" extensions:"x-order=2"`
+}
