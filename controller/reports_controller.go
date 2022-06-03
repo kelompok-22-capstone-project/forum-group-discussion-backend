df8ab9959a2a62d5e8a8e32c1ddb56c0f01b09b6
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-03 10:55:59 +0700

    Add an endpoint to get all report

--- controller/reports_controller.go
+++ controller/reports_controller.go
@@ -11,6 +11,7 @@ func NewReportsController() *reportsController {
 func (r *reportsController) Route(g *echo.Group) {
 	group := g.Group("/reports")
 	group.POST("", r.postCreateReport)
+	group.GET("", r.getReports)
 }
 
 // postCreateReport godoc
@@ -29,4 +30,49 @@ func (r *reportsController) Route(g *echo.Group) {
 // @Router       /reports [post]
 func (r *reportsController) postCreateReport(c echo.Context) error {
 	return nil
+}
+
+// getReports     godoc
+// @Summary      Get Reports
+// @Description  This endpoint is used to get all report
+// @Tags         reports
+// @Produce      json
+// @Param        status  query  string  false  "options: all, review, rejected, accepted, default all"
+// @Param        page    query  int     false  "page, default 1"
+// @Param        limit   query  int     false  "limit, default 20"
+// @Security     ApiKeyAuth
+// @Success      200  {object}  reportsResponse
+// @Failure      500  {object}  echo.HTTPError
+// @Router       /reports [get]
+func (r *reportsController) getReports(c echo.Context) error {
+	_ = c.QueryParam("status")
+	_ = c.QueryParam("page")
+	_ = c.QueryParam("limit")
+	return nil
+}
+
+// reportsResponse struct is used for swaggo to generate the API documentation, as it doesn't support generic yet.
+type reportsResponse struct {
+	Status  string      `json:"status" extensions:"x-order=0"`
+	Message string      `json:"message" extensions:"x-order=1"`
+	Data    reportsData `json:"data" extensions:"x-order=2"`
+}
+
+type reportsData struct {
+	Reports  []reportData `json:"reports" extensions:"x-order=0"`
+	PageInfo pageInfoData `json:"pageInfo" extensions:"x-order=1"`
+}
+
+type reportData struct {
+	ID                string `json:"ID" extensions:"x-order=0"`
+	ModeratorID       string `json:"moderatorID" extensions:"x-order=1"`
+	ModeratorUsername string `json:"moderatorUsername" extensions:"x-order=2"`
+	ModeratorName     string `json:"moderatorName" extensions:"x-order=3"`
+	UserID            string `json:"userID" extensions:"x-order=4"`
+	Username          string `json:"username" extensions:"x-order=5"`
+	Name              string `json:"name" extensions:"x-order=6"`
+	Reason            string `json:"reason" extensions:"x-order=7"`
+	Status            string `json:"status" extensions:"x-order=8"`
+	// ReportedOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
+	ReportedOn string `json:"reportedOn" extensions:"x-order=9"`
 }
