45b25ee75f6be54e136a4bad8aeeb63e286898cb
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-11 20:40:00 +0700

    Create thread entity

--- entity/thread.go
+++ entity/thread.go
@@ -3,12 +3,18 @@ package entity
 import "time"
 
 type Thread struct {
-	ID          string
-	Title       string
-	Description string
-	TotalViewer uint64
-	Creator     User
-	Category    Category
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	ID            string
+	Title         string
+	Description   string
+	TotalViewer   uint64
+	TotalLike     uint64
+	TotalFollower uint64
+	TotalComment  uint64
+	Creator       User
+	Category      Category
+	IsLiked       bool
+	IsFollowed    bool
+	Moderators    []Moderator
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
 }
