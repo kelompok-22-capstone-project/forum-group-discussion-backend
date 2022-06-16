b8f8a71a89602930bedc7beec8d2ec844d0bbdd6
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-16 11:24:54 +0700

    Add Create method

--- service/thread/thread_service.go
+++ service/thread/thread_service.go
@@ -3,6 +3,7 @@ package thread
 import (
 	"context"
 
+	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/model/payload"
 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/model/response"
 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/utils/generator"
 )
@@ -15,4 +16,10 @@ type ThreadService interface {
 		limit uint,
 		query string,
 	) (rs response.Pagination[response.ManyThread], err error)
+
+	Create(
+		ctx context.Context,
+		tp generator.TokenPayload,
+		p payload.CreateThread,
+	) (id string, err error)
 }
