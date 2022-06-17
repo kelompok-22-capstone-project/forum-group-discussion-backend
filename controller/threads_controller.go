a38247feb78d6710c80004a5212b3f86846164ea
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-17 11:01:10 +0700

    Remove unnecessary code

--- controller/threads_controller.go
+++ controller/threads_controller.go
@@ -327,16 +327,6 @@ type commentsResponse struct {
 }
 
 type commentsInfoWrapper struct {
-	Threads  []commentData `json:"list" extensions:"x-order=0"`
-	PageInfo pageInfoData  `json:"pageInfo" extensions:"x-order=1"`
-}
-
-type commentData struct {
-	ID       string `json:"ID" extensions:"x-order=0"`
-	UserID   string `json:"userID" extensions:"x-order=1"`
-	Username string `json:"username" extensions:"x-order=2"`
-	Name     string `json:"name" extensions:"x-order=3"`
-	Comment  string `json:"comment" extensions:"x-order=4"`
-	// PublishedOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
-	PublishedOn string `json:"publishedOn" extensions:"x-order=5"`
+	Threads  []response.Comment `json:"list" extensions:"x-order=0"`
+	PageInfo pageInfoData       `json:"pageInfo" extensions:"x-order=1"`
 }
