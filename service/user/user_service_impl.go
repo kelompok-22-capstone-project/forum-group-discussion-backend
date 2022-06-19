5dc1076135f7783bd95b58f9d48daec7408c1c88
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-19 20:28:47 +0700

    Implements GetAll method to get all users

--- service/user/user_service_impl.go
+++ service/user/user_service_impl.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"net/mail"
+	"time"
 
 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/entity"
 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/model/payload"
@@ -117,6 +118,74 @@ func (u *userServiceImpl) Login(ctx context.Context, p payload.Login) (r respons
 
 	r.Token = token
 	r.Role = user.Role
+
+	return
+}
+
+func (u *userServiceImpl) GetAll(
+	ctx context.Context,
+	accessorUserID,
+	orderBy,
+	status string,
+	page,
+	limit uint,
+) (r response.Pagination[response.User], err error) {
+	if page == 0 {
+		page = 1
+	}
+
+	if limit == 0 {
+		limit = 20
+	}
+
+	var userOrderBy entity.UserOrderBy
+	var userStatus entity.UserStatus
+
+	if orderBy == "ranking" {
+		userOrderBy = entity.Ranking
+	} else {
+		userOrderBy = entity.RegisteredDate
+	}
+
+	if status == "banned" {
+		userStatus = entity.Banned
+	} else {
+		userStatus = entity.Active
+	}
+
+	pageInfo := entity.PageInfo{
+		Page:  page,
+		Limit: limit,
+	}
+
+	pagination, dbErr := u.userRepository.FindAllWithStatusAndPagination(ctx, accessorUserID, userOrderBy, userStatus, pageInfo)
+	if dbErr != nil {
+		err = service.MapError(dbErr)
+		return
+	}
+
+	r.List = make([]response.User, len(pagination.List))
+
+	r.PageInfo.Page = pagination.PageInfo.Page
+	r.PageInfo.Limit = pagination.PageInfo.Limit
+	r.PageInfo.Total = pagination.PageInfo.Total
+	r.PageInfo.PageTotal = pagination.PageInfo.PageTotal
+
+	for i, user := range pagination.List {
+		user := response.User{
+			UserID:        user.ID,
+			Username:      user.Username,
+			Email:         user.Email,
+			Name:          user.Name,
+			Role:          user.Role,
+			IsActive:      user.IsActive,
+			RegisteredOn:  user.CreatedAt.Format(time.RFC822),
+			TotalThread:   uint(user.TotalThread),
+			TotalFollower: uint(user.TotalFollower),
+			IsFollowed:    user.IsFollowed,
+		}
+		r.List[i] = user
+	}
 
 	return
 }
