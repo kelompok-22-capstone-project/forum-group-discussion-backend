294cc2b21cbd622b445773217e6a19c5f657aaf4
Sari Fatur <sarifaturr14@gmail.com>
2022-06-19 12:46:42 +0700

    Create testing for GetAllByCategory

--- service/category/category_service_impl_test.go
+++ service/category/category_service_impl_test.go
@@ -14,7 +14,6 @@ import (
 	mcr "github.com/kelompok-22-capstone-project/forum-group-discussion-backend/repository/category/mocks"
 	mtr "github.com/kelompok-22-capstone-project/forum-group-discussion-backend/repository/thread/mocks"
 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/service"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/utils/generator"
 	mig "github.com/kelompok-22-capstone-project/forum-group-discussion-backend/utils/generator/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -85,6 +84,39 @@ func TestGetAll(t *testing.T) {
 				).Once()
 			},
 		},
+		{
+			name:          "it should return valid categories, when repository return nil error",
+			expectedError: nil,
+			expectedCategories: []response.Category{
+				{
+					ID:          "c-abc",
+					Name:        "Tech",
+					Description: "This is tech category.",
+					CreatedOn:   now.Format(time.RFC822),
+				},
+			},
+			mockBehaviour: func() {
+				mockRepo.On(
+					"FindAll",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+				).Return(
+					func(ctx context.Context) []entity.Category {
+						return []entity.Category{
+							{
+								ID:          "c-abc",
+								Name:        "Tech",
+								Description: "This is tech category.",
+								CreatedAt:   now,
+								UpdatedAt:   now,
+							},
+						}
+					},
+					func(ctx context.Context) error {
+						return nil
+					},
+				).Once()
+			},
+		},
 	}
 
 	for _, testCase := range testCases {
@@ -112,7 +144,7 @@ func TestCreate(t *testing.T) {
 	testCases := []struct {
 		name              string
 		inputPayload      payload.CreateCategory
-		inputTokenPayload generator.TokenPayload
+		inputAccessorRole string
 		expectedError     error
 		expectedID        string
 		mockBehaviour     func()
@@ -123,15 +155,10 @@ func TestCreate(t *testing.T) {
 				Name:        "Music",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "user",
-				Role:     "user",
-				IsActive: true,
-			},
-			expectedError: service.ErrAccessForbidden,
-			expectedID:    "",
-			mockBehaviour: func() {},
+			inputAccessorRole: "user",
+			expectedError:     service.ErrAccessForbidden,
+			expectedID:        "",
+			mockBehaviour:     func() {},
 		},
 		{
 			name: "it should return service.ErrInvalidPayload, when payload is invalid",
@@ -139,15 +166,10 @@ func TestCreate(t *testing.T) {
 				Name:        "M",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			expectedError: service.ErrInvalidPayload,
-			expectedID:    "",
-			mockBehaviour: func() {},
+			inputAccessorRole: "admin",
+			expectedError:     service.ErrInvalidPayload,
+			expectedID:        "",
+			mockBehaviour:     func() {},
 		},
 		{
 			name: "it should return service.ErrRepository, when idGenerator return an error",
@@ -155,14 +177,9 @@ func TestCreate(t *testing.T) {
 				Name:        "Music",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			expectedError: service.ErrRepository,
-			expectedID:    "",
+			inputAccessorRole: "admin",
+			expectedError:     service.ErrRepository,
+			expectedID:        "",
 			mockBehaviour: func() {
 				mockIDGen.On(
 					"GenerateCategoryID",
@@ -182,14 +199,9 @@ func TestCreate(t *testing.T) {
 				Name:        "Music",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			expectedError: service.ErrRepository,
-			expectedID:    "",
+			inputAccessorRole: "admin",
+			expectedError:     service.ErrRepository,
+			expectedID:        "",
 			mockBehaviour: func() {
 				mockIDGen.On(
 					"GenerateCategoryID",
@@ -219,14 +231,9 @@ func TestCreate(t *testing.T) {
 				Name:        "Music",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			expectedError: nil,
-			expectedID:    "d-adsf",
+			inputAccessorRole: "admin",
+			expectedError:     nil,
+			expectedID:        "d-adsf",
 			mockBehaviour: func() {
 				mockIDGen.On(
 					"GenerateCategoryID",
@@ -256,7 +263,7 @@ func TestCreate(t *testing.T) {
 		t.Run(testCase.name, func(t *testing.T) {
 			testCase.mockBehaviour()
 
-			id, err := categoryService.Create(context.Background(), testCase.inputTokenPayload, testCase.inputPayload)
+			id, err := categoryService.Create(context.Background(), testCase.inputAccessorRole, testCase.inputPayload)
 
 			if testCase.expectedError != nil {
 				assert.ErrorIs(t, err, testCase.expectedError)
@@ -277,7 +284,7 @@ func TestUpdate(t *testing.T) {
 	testCases := []struct {
 		name              string
 		inputPayload      payload.UpdateCategory
-		inputTokenPayload generator.TokenPayload
+		inputAccessorRole string
 		inputID           string
 		expectedError     error
 		mockBehaviour     func()
@@ -288,15 +295,10 @@ func TestUpdate(t *testing.T) {
 				Name:        "Music",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "user",
-				Role:     "user",
-				IsActive: true,
-			},
-			inputID:       "",
-			expectedError: service.ErrAccessForbidden,
-			mockBehaviour: func() {},
+			inputAccessorRole: "user",
+			inputID:           "",
+			expectedError:     service.ErrAccessForbidden,
+			mockBehaviour:     func() {},
 		},
 		{
 			name: "it should return service.ErrInvalidPayload, when payload is invalid",
@@ -304,15 +306,10 @@ func TestUpdate(t *testing.T) {
 				Name:        "M",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			inputID:       "",
-			expectedError: service.ErrInvalidPayload,
-			mockBehaviour: func() {},
+			inputAccessorRole: "admin",
+			inputID:           "",
+			expectedError:     service.ErrInvalidPayload,
+			mockBehaviour:     func() {},
 		},
 		{
 			name: "it should return service.ErrRepository, when repository.ErrDatabase return an error",
@@ -320,14 +317,9 @@ func TestUpdate(t *testing.T) {
 				Name:        "Music",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			inputID:       "",
-			expectedError: service.ErrRepository,
+			inputAccessorRole: "admin",
+			inputID:           "",
+			expectedError:     service.ErrRepository,
 			mockBehaviour: func() {
 				mockRepo.On(
 					"Update",
@@ -347,14 +339,9 @@ func TestUpdate(t *testing.T) {
 				Name:        "Music",
 				Description: "This is a music description",
 			},
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			inputID:       "",
-			expectedError: nil,
+			inputAccessorRole: "admin",
+			inputID:           "",
+			expectedError:     nil,
 			mockBehaviour: func() {
 				mockRepo.On(
 					"Update",
@@ -374,7 +361,7 @@ func TestUpdate(t *testing.T) {
 		t.Run(testCase.name, func(t *testing.T) {
 			testCase.mockBehaviour()
 
-			err := categoryService.Update(context.Background(), testCase.inputTokenPayload, testCase.inputID, testCase.inputPayload)
+			err := categoryService.Update(context.Background(), testCase.inputAccessorRole, testCase.inputID, testCase.inputPayload)
 
 			if testCase.expectedError != nil {
 				assert.ErrorIs(t, err, testCase.expectedError)
@@ -394,33 +381,22 @@ func TestDelete(t *testing.T) {
 
 	testCases := []struct {
 		name              string
-		inputTokenPayload generator.TokenPayload
+		inputAccessorRole string
 		inputID           string
 		expectedError     error
 		mockBehaviour     func()
 	}{
 		{
-			name: "it should return service.ErrAccessForbidden, when role is not admin",
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "user",
-				Role:     "user",
-				IsActive: true,
-			},
-			expectedError: service.ErrAccessForbidden,
-			mockBehaviour: func() {},
+			name:              "it should return service.ErrAccessForbidden, when role is not admin",
+			inputAccessorRole: "user",
+			expectedError:     service.ErrAccessForbidden,
+			mockBehaviour:     func() {},
 		},
 		{
-			name: "it should return service.ErrRepository, when repository.ErrDatabase return an error",
-
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			inputID:       "",
-			expectedError: service.ErrRepository,
+			name:              "it should return service.ErrRepository, when repository.ErrDatabase return an error",
+			inputAccessorRole: "admin",
+			inputID:           "",
+			expectedError:     service.ErrRepository,
 			mockBehaviour: func() {
 				mockRepo.On(
 					"Delete",
@@ -434,16 +410,10 @@ func TestDelete(t *testing.T) {
 			},
 		},
 		{
-			name: "it should return nil error,  when no error is returned",
-
-			inputTokenPayload: generator.TokenPayload{
-				ID:       "u-abcd",
-				Username: "admin",
-				Role:     "admin",
-				IsActive: true,
-			},
-			inputID:       "",
-			expectedError: nil,
+			name:              "it should return nil error,  when no error is returned",
+			inputAccessorRole: "admin",
+			inputID:           "",
+			expectedError:     nil,
 			mockBehaviour: func() {
 				mockRepo.On(
 					"Delete",
@@ -462,7 +432,7 @@ func TestDelete(t *testing.T) {
 		t.Run(testCase.name, func(t *testing.T) {
 			testCase.mockBehaviour()
 
-			err := categoryService.Delete(context.Background(), testCase.inputTokenPayload, testCase.inputID)
+			err := categoryService.Delete(context.Background(), testCase.inputAccessorRole, testCase.inputID)
 
 			if testCase.expectedError != nil {
 				assert.ErrorIs(t, err, testCase.expectedError)
@@ -474,5 +444,208 @@ func TestDelete(t *testing.T) {
 }
 
 func TestGetAllByCategory(t *testing.T) {
+	mockRepo := &mcr.CategoryRepository{}
+	mockThreadRepo := &mtr.ThreadRepository{}
+	mockIDGen := &mig.IDGenerator{}
+	now := time.Now()
 
+	var categoryService CategoryService = NewCategoryServiceImpl(mockRepo, mockThreadRepo, mockIDGen)
+
+	testCases := []struct {
+		name               string
+		inputAccessorID    string
+		inputCategoryID    string
+		inputPage          uint
+		inputLimit         uint
+		expectedError      error
+		expectedPagination response.Pagination[response.ManyThread]
+		mockBehaviour      func()
+	}{
+		{
+			name:               "it should return service.ErrRepository, when repository.ErrDatabase return an error",
+			inputAccessorID:    "",
+			inputCategoryID:    "",
+			inputPage:          0,
+			inputLimit:         0,
+			expectedError:      service.ErrRepository,
+			expectedPagination: response.Pagination[response.ManyThread]{},
+			mockBehaviour: func() {
+				mockRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, categoryID string) entity.Category {
+						return entity.Category{}
+					},
+					func(ctx context.Context, categoryID string) error {
+						return repository.ErrDatabase
+					},
+				).Once()
+			},
+		},
+		{
+			name:               "it should return service.ErrRepository, when repository.ErrDatabase return an error",
+			inputAccessorID:    "",
+			inputCategoryID:    "",
+			inputPage:          1,
+			inputLimit:         10,
+			expectedError:      service.ErrRepository,
+			expectedPagination: response.Pagination[response.ManyThread]{},
+			mockBehaviour: func() {
+				mockRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, categoryID string) entity.Category {
+						return entity.Category{}
+					},
+					func(ctx context.Context, categoryID string) error {
+						return nil
+					},
+				).Once()
+
+				mockThreadRepo.On(
+					"FindAllByCategoryIDWithPagination",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+					mock.AnythingOfType(fmt.Sprintf("%T", entity.PageInfo{})),
+				).Return(
+					func(
+						ctx context.Context,
+						accessorUserID string,
+						categoryID string,
+						pageInfo entity.PageInfo) entity.Pagination[entity.Thread] {
+						return entity.Pagination[entity.Thread]{}
+					},
+					func(
+						ctx context.Context,
+						accessorUserID string,
+						categoryID string,
+						pageInfo entity.PageInfo) error {
+						return repository.ErrDatabase
+					},
+				).Once()
+			},
+		},
+		{
+			name:            "it should return valid categories, when repository return nil error",
+			inputAccessorID: "",
+			inputCategoryID: "",
+			inputPage:       1,
+			inputLimit:      10,
+			expectedError:   nil,
+			expectedPagination: response.Pagination[response.ManyThread]{
+				List: []response.ManyThread{
+					{
+						ID:              "d-Casfkj",
+						Title:           "Technology",
+						CategoryID:      "g-jMds",
+						CategoryName:    "Tech",
+						PublishedOn:     now.Format(time.RFC822),
+						IsLiked:         false,
+						IsFollowed:      false,
+						Description:     "Technology is the result of accumulated knowledge and application of skills, methods, and processes used in industrial production and scientific research.",
+						TotalViewer:     234,
+						TotalLike:       243,
+						TotalFollower:   674,
+						TotalComment:    23,
+						CreatorID:       "d-MDje",
+						CreatorUsername: "erikrio",
+						CreatorName:     "erik",
+					},
+				},
+				PageInfo: response.PageInfo{
+					Limit:     10,
+					Page:      1,
+					PageTotal: 1,
+					Total:     1,
+				},
+			},
+			mockBehaviour: func() {
+				mockRepo.On(
+					"FindByID",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+				).Return(
+					func(ctx context.Context, categoryID string) entity.Category {
+						return entity.Category{}
+					},
+					func(ctx context.Context, categoryID string) error {
+						return nil
+					},
+				).Once()
+
+				mockThreadRepo.On(
+					"FindAllByCategoryIDWithPagination",
+					mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+					mock.AnythingOfType(fmt.Sprintf("%T", "")),
+					mock.AnythingOfType(fmt.Sprintf("%T", entity.PageInfo{})),
+				).Return(
+					func(
+						ctx context.Context,
+						accessorUserID string,
+						categoryID string,
+						pageInfo entity.PageInfo) entity.Pagination[entity.Thread] {
+						return entity.Pagination[entity.Thread]{
+							List: []entity.Thread{
+								{
+									ID:            "d-Casfkj",
+									Title:         "Technology",
+									Description:   "Technology is the result of accumulated knowledge and application of skills, methods, and processes used in industrial production and scientific research.",
+									TotalViewer:   234,
+									TotalLike:     243,
+									TotalFollower: 674,
+									TotalComment:  23,
+									Creator: entity.User{
+										ID:       "d-MDje",
+										Username: "erikrio",
+										Name:     "erik",
+									},
+									Category: entity.Category{
+										ID:   "g-jMds",
+										Name: "Tech",
+									},
+									IsLiked:    false,
+									IsFollowed: false,
+									CreatedAt:  now,
+								},
+							},
+							PageInfo: entity.PageInfo{
+								Limit:     10,
+								Page:      1,
+								PageTotal: 1,
+								Total:     1,
+							},
+						}
+					},
+					func(
+						ctx context.Context,
+						accessorUserID string,
+						categoryID string,
+						pageInfo entity.PageInfo) error {
+						return nil
+					},
+				).Once()
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			testCase.mockBehaviour()
+
+			pagination, err := categoryService.GetAllByCategory(context.Background(), testCase.inputAccessorID, testCase.inputCategoryID, testCase.inputPage, testCase.inputLimit)
+
+			if testCase.expectedError != nil {
+				assert.ErrorIs(t, err, testCase.expectedError)
+			} else {
+				assert.ElementsMatch(t, pagination.List, testCase.expectedPagination.List)
+				assert.Equal(t, pagination.PageInfo, testCase.expectedPagination.PageInfo)
+			}
+		})
+	}
 }
