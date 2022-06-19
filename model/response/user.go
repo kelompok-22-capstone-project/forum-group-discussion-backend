4389ef32a2b87f7983a7759ad26516a0a65ae1f0
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-19 20:27:23 +0700

    Create user model

---
+++ model/response/user.go
@@ -0,0 +1,15 @@
+package response
+
+type User struct {
+	UserID   string `json:"userID" extensions:"x-order=0"`
+	Username string `json:"username" extensions:"x-order=1"`
+	Email    string `json:"email" extensions:"x-order=2"`
+	Name     string `json:"name" extensions:"x-order=3"`
+	Role     string `json:"role" extensions:"x-order=4"`
+	IsActive bool   `json:"isActive" extensions:"x-order=5"`
+	// RegisteredOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
+	RegisteredOn  string `json:"registeredOn" extensions:"x-order=6"`
+	TotalThread   uint   `json:"totalThread" extensions:"x-order=7"`
+	TotalFollower uint   `json:"totalFollower" extensions:"x-order=8"`
+	IsFollowed    bool   `json:"isFollowed" extensions:"x-order=9"`
+}
