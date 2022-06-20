2fd6758e99eb45823e66b0d9e3d8133151230d3f
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-20 18:33:53 +0700

    Refactor the code

--- model/payload/create_report.go
+++ model/payload/create_report.go
@@ -1,6 +1,7 @@
 package payload
 
 type CreateReport struct {
-	UserID string `json:"userID" validate:"nonzero,min=2,max=10" extensions:"x-order=0"`
-	Reason string `json:"reason" validate:"nonzero,min=2" extensions:"x-order=1"`
+	UserID   string `json:"userID" validate:"nonzero,min=2,max=10" extensions:"x-order=0"`
+	ThreadID string `json:"threadID" validate:"nonzero,min=2,max=10" extensions:"x-order=1"`
+	Reason   string `json:"reason" validate:"nonzero,min=2" extensions:"x-order=2"`
 }
