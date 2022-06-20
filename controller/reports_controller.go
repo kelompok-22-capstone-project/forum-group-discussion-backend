de6ecbc8006765e2c515bce3e4938fed7f67c22e
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-20 21:52:44 +0700

    Remove unnecessary code

--- controller/reports_controller.go
+++ controller/reports_controller.go
@@ -1,6 +1,9 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/model/response"
+	"github.com/labstack/echo/v4"
+)
 
 type reportsController struct{}
 
@@ -61,20 +64,6 @@ type reportsResponse struct {
 }
 
 type reportsData struct {
-	Reports  []reportData `json:"list" extensions:"x-order=0"`
-	PageInfo pageInfoData `json:"pageInfo" extensions:"x-order=1"`
-}
-
-type reportData struct {
-	ID                string `json:"ID" extensions:"x-order=0"`
-	ModeratorID       string `json:"moderatorID" extensions:"x-order=1"`
-	ModeratorUsername string `json:"moderatorUsername" extensions:"x-order=2"`
-	ModeratorName     string `json:"moderatorName" extensions:"x-order=3"`
-	UserID            string `json:"userID" extensions:"x-order=4"`
-	Username          string `json:"username" extensions:"x-order=5"`
-	Name              string `json:"name" extensions:"x-order=6"`
-	Reason            string `json:"reason" extensions:"x-order=7"`
-	Status            string `json:"status" extensions:"x-order=8"`
-	// ReportedOn layout format: time.RFC822 (02 Jan 06 15:04 MST)
-	ReportedOn string `json:"reportedOn" extensions:"x-order=9"`
+	Reports  []response.Report `json:"list" extensions:"x-order=0"`
+	PageInfo pageInfoData      `json:"pageInfo" extensions:"x-order=1"`
 }
