a8d72de47a976b1522ff43a38b9a604dd60fa288
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-21 10:46:39 +0700

    Implements an endpoint to get the admin dashboard info

--- controller/admin_controller.go
+++ controller/admin_controller.go
@@ -1,19 +1,31 @@
 package controller
 
 import (
+	"net/http"
+
+	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/middleware"
+	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/model"
 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/model/response"
+	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/service/admin"
+	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/utils/generator"
 	"github.com/labstack/echo/v4"
 )
 
-type adminController struct{}
+type adminController struct {
+	service        admin.AdminService
+	tokenGenerator generator.TokenGenerator
+}
 
-func NewAdminController() *adminController {
-	return &adminController{}
+func NewAdminController(
+	service admin.AdminService,
+	tokenGenerator generator.TokenGenerator,
+) *adminController {
+	return &adminController{service: service, tokenGenerator: tokenGenerator}
 }
 
 func (i *adminController) Route(g *echo.Group) {
 	group := g.Group("/admin/dashboard")
-	group.GET("/", i.getInfo)
+	group.GET("/", i.getInfo, middleware.JWTMiddleware())
 }
 
 // getInfo     godoc
@@ -27,7 +39,16 @@ func (i *adminController) Route(g *echo.Group) {
 // @Failure      500  {object}  echo.HTTPError
 // @Router       /admin/dashboard [get]
 func (i *adminController) getInfo(c echo.Context) error {
-	return nil
+	tp := i.tokenGenerator.ExtractToken(c)
+
+	infoResponse, err := i.service.GetDashboardInfo(c.Request().Context(), tp.Role)
+	if err != nil {
+		return newErrorResponse(err)
+	}
+
+	response := model.NewResponse("Success", "Get admin dashboard info successful.", infoResponse)
+
+	return c.JSON(http.StatusOK, response)
 }
 
 // profileResponse struct is used for swaggo to generate the API documentation, as it doesn't support generic yet.
