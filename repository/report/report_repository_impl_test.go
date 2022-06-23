9b10688577c5ef25708da9c49ec21d08b9a6fb58
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-23 08:05:13 +0700

    Comment unnecessary code

--- repository/report/report_repository_impl_test.go
+++ repository/report/report_repository_impl_test.go
@@ -1,55 +1,55 @@
 package report
 
-import (
-	"context"
-	"database/sql"
-	"log"
-	"testing"
+// import (
+// 	"context"
+// 	"database/sql"
+// 	"log"
+// 	"testing"
 
-	"github.com/joho/godotenv"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/config"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/entity"
-)
+// 	"github.com/joho/godotenv"
+// 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/config"
+// 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/entity"
+// )
 
-var db *sql.DB
+// var db *sql.DB
 
-func init() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	err := godotenv.Load("./../../.env.example")
-	if err != nil {
-		panic(err)
-	}
+// func init() {
+// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+// 	err := godotenv.Load("./../../.env.example")
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	db, err = config.NewPostgreSQLDatabase()
-	if err != nil {
-		panic(err)
-	}
-}
+// 	db, err = config.NewPostgreSQLDatabase()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
 
-func TestGetReportsWithPagination(t *testing.T) {
-	var repo ReportRepository = NewReportRepositoryImpl(db)
+// func TestGetReportsWithPagination(t *testing.T) {
+// 	var repo ReportRepository = NewReportRepositoryImpl(db)
 
-	pageInfo := entity.PageInfo{Page: 1, Limit: 10}
-	reportStatus := entity.Review
+// 	pageInfo := entity.PageInfo{Page: 1, Limit: 10}
+// 	reportStatus := entity.Review
 
-	if pagination, err := repo.GetReportsWithPagination(context.Background(), pageInfo, reportStatus); err != nil {
-		t.Fatalf("Error happened: %+v", err)
-	} else {
-		t.Logf("Pagination: %+v", pagination)
-	}
-}
+// 	if pagination, err := repo.GetReportsWithPagination(context.Background(), pageInfo, reportStatus); err != nil {
+// 		t.Fatalf("Error happened: %+v", err)
+// 	} else {
+// 		t.Logf("Pagination: %+v", pagination)
+// 	}
+// }
 
-func TestInsert(t *testing.T) {
-	var repo ReportRepository = NewReportRepositoryImpl(db)
+// func TestInsert(t *testing.T) {
+// 	var repo ReportRepository = NewReportRepositoryImpl(db)
 
-	ID := "b-abcdefg"
-	moderatorID := "m-1234"
-	userID := "u-NplCrv"
-	reason := "Bocil ini sangat meresahkan, tolong di banned min."
+// 	ID := "b-abcdefg"
+// 	moderatorID := "m-1234"
+// 	userID := "u-NplCrv"
+// 	reason := "Bocil ini sangat meresahkan, tolong di banned min."
 
-	if err := repo.Insert(context.Background(), ID, moderatorID, userID, reason); err != nil {
-		t.Fatalf("Error happened: %+v", err)
-	} else {
-		t.Logf("Successfully inserted report with ID %s", ID)
-	}
-}
+// 	if err := repo.Insert(context.Background(), ID, moderatorID, userID, reason); err != nil {
+// 		t.Fatalf("Error happened: %+v", err)
+// 	} else {
+// 		t.Logf("Successfully inserted report with ID %s", ID)
+// 	}
+// }
--- service/thread/thread_service_impl_test.go
+++ service/thread/thread_service_impl_test.go
@@ -1,208 +1,208 @@
 package thread
 
-import (
-	"context"
-	"database/sql"
-	"log"
-	"testing"
+// import (
+// 	"context"
+// 	"database/sql"
+// 	"log"
+// 	"testing"
 
-	"github.com/joho/godotenv"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/config"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/model/payload"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/repository/category"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/repository/thread"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/repository/user"
-	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/utils/generator"
-)
+// 	"github.com/joho/godotenv"
+// 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/config"
+// 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/model/payload"
+// 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/repository/category"
+// 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/repository/thread"
+// 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/repository/user"
+// 	"github.com/kelompok-22-capstone-project/forum-group-discussion-backend/utils/generator"
+// )
 
-var db *sql.DB
-var threadRepo thread.ThreadRepository
-var categoryRepo category.CategoryRepository
-var userRepo user.UserRepository
-var idGenerator generator.IDGenerator
+// var db *sql.DB
+// var threadRepo thread.ThreadRepository
+// var categoryRepo category.CategoryRepository
+// var userRepo user.UserRepository
+// var idGenerator generator.IDGenerator
 
-func init() {
-	godotenv.Load("./../../.env.example")
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	var err error
-	if db, err = config.NewPostgreSQLDatabase(); err != nil {
-		panic(err)
-	}
+// func init() {
+// 	godotenv.Load("./../../.env.example")
+// 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+// 	var err error
+// 	if db, err = config.NewPostgreSQLDatabase(); err != nil {
+// 		panic(err)
+// 	}
 
-	threadRepo = thread.NewThreadRepositoryImpl(db)
-	categoryRepo = category.NewCategoryRepositoryImpl(db)
-	userRepo = user.NewUserRepositoryImpl(db)
-	idGenerator = generator.NewNanoidIDGenerator()
-}
+// 	threadRepo = thread.NewThreadRepositoryImpl(db)
+// 	categoryRepo = category.NewCategoryRepositoryImpl(db)
+// 	userRepo = user.NewUserRepositoryImpl(db)
+// 	idGenerator = generator.NewNanoidIDGenerator()
+// }
 
-func TestGetAll(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestGetAll(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	accessorUserID := "u-ZrxmQS"
+// 	accessorUserID := "u-ZrxmQS"
 
-	page := 1
-	limit := 20
-	query := "Read"
+// 	page := 1
+// 	limit := 20
+// 	query := "Read"
 
-	if pagination, err := service.GetAll(context.Background(), accessorUserID, uint(page), uint(limit), query); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Pagination: %+v", pagination)
-	}
-}
+// 	if pagination, err := service.GetAll(context.Background(), accessorUserID, uint(page), uint(limit), query); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Pagination: %+v", pagination)
+// 	}
+// }
 
-func TestCreate(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestCreate(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	accessorUserID := "u-ZrxmQS"
+// 	accessorUserID := "u-ZrxmQS"
 
-	p := payload.CreateThread{
-		Title:       "Go Programming Language Going Hype",
-		Description: "Currently Go Programming Language going hype, because it features.",
-		CategoryID:  "c-abc",
-	}
+// 	p := payload.CreateThread{
+// 		Title:       "Go Programming Language Going Hype",
+// 		Description: "Currently Go Programming Language going hype, because it features.",
+// 		CategoryID:  "c-abc",
+// 	}
 
-	if id, err := service.Create(context.Background(), accessorUserID, p); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Successfully create a thread with ID %s", id)
-	}
-}
+// 	if id, err := service.Create(context.Background(), accessorUserID, p); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Successfully create a thread with ID %s", id)
+// 	}
+// }
 
-func TestGetByID(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestGetByID(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	accessorUserID := "u-ZrxmQS"
+// 	accessorUserID := "u-ZrxmQS"
 
-	id := "t-kxdoB7i"
+// 	id := "t-kxdoB7i"
 
-	if thread, err := service.GetByID(context.Background(), accessorUserID, id); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Thread: %+v", thread)
-	}
-}
+// 	if thread, err := service.GetByID(context.Background(), accessorUserID, id); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Thread: %+v", thread)
+// 	}
+// }
 
-func TestUpdate(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestUpdate(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	accessorUserID := "u-ZrxmQS"
+// 	accessorUserID := "u-ZrxmQS"
 
-	id := "t-kxdoB7i"
+// 	id := "t-kxdoB7i"
 
-	p := payload.UpdateThread{
-		Title:       "Go Programming Going Hype",
-		Description: "Currently Go Programming going hype, because it features.",
-		CategoryID:  "c-def",
-	}
+// 	p := payload.UpdateThread{
+// 		Title:       "Go Programming Going Hype",
+// 		Description: "Currently Go Programming going hype, because it features.",
+// 		CategoryID:  "c-def",
+// 	}
 
-	if err := service.Update(context.Background(), accessorUserID, id, p); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Successfully updated a thread with ID %s", id)
-	}
-}
+// 	if err := service.Update(context.Background(), accessorUserID, id, p); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Successfully updated a thread with ID %s", id)
+// 	}
+// }
 
-func TestDelete(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestDelete(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	accessorUserID := "u-ZrxmQS"
-	role := "user"
+// 	accessorUserID := "u-ZrxmQS"
+// 	role := "user"
 
-	id := "t-45uolpR"
+// 	id := "t-45uolpR"
 
-	if err := service.Delete(context.Background(), accessorUserID, role, id); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Successfully deleted a thread with ID %s", id)
-	}
-}
+// 	if err := service.Delete(context.Background(), accessorUserID, role, id); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Successfully deleted a thread with ID %s", id)
+// 	}
+// }
 
-func TestGetComments(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestGetComments(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	threadID := "t-abcdefg"
+// 	threadID := "t-abcdefg"
 
-	if pagination, err := service.GetComments(context.Background(), threadID, 0, 0); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Pagination: %+v", pagination)
-	}
-}
+// 	if pagination, err := service.GetComments(context.Background(), threadID, 0, 0); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Pagination: %+v", pagination)
+// 	}
+// }
 
-func TestCreateComment(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestCreateComment(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	threadID := "t-abcdefg"
-	accessorUserID := "u-6fj7Jo"
+// 	threadID := "t-abcdefg"
+// 	accessorUserID := "u-6fj7Jo"
 
-	p := payload.CreateComment{
-		Comment: "Great post that i've ever seen. Good job.",
-	}
+// 	p := payload.CreateComment{
+// 		Comment: "Great post that i've ever seen. Good job.",
+// 	}
 
-	if id, err := service.CreateComment(context.Background(), threadID, accessorUserID, p); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Successfully inserted comment with ID %s", id)
-	}
-}
+// 	if id, err := service.CreateComment(context.Background(), threadID, accessorUserID, p); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Successfully inserted comment with ID %s", id)
+// 	}
+// }
 
-func TestChangeFollowingState(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestChangeFollowingState(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	threadID := "t-abcdefg"
-	accessorUserID := "u-kt56R1"
+// 	threadID := "t-abcdefg"
+// 	accessorUserID := "u-kt56R1"
 
-	if err := service.ChangeFollowingState(context.Background(), threadID, accessorUserID); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Log("Successfully change the following state")
-	}
-}
+// 	if err := service.ChangeFollowingState(context.Background(), threadID, accessorUserID); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Log("Successfully change the following state")
+// 	}
+// }
 
-func TestChangeLikeState(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestChangeLikeState(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	threadID := "t-abcdefg"
-	accessorUserID := "u-kt56R1"
+// 	threadID := "t-abcdefg"
+// 	accessorUserID := "u-kt56R1"
 
-	if err := service.ChangeLikeState(context.Background(), threadID, accessorUserID); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Log("Successfully change the like state")
-	}
-}
+// 	if err := service.ChangeLikeState(context.Background(), threadID, accessorUserID); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Log("Successfully change the like state")
+// 	}
+// }
 
-func TestAddModerator(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestAddModerator(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	p := payload.AddRemoveModerator{
-		Username: "rezana",
-	}
+// 	p := payload.AddRemoveModerator{
+// 		Username: "rezana",
+// 	}
 
-	threadID := "t-abcdefg"
-	accessorUserID := "u-ZrxmQS"
+// 	threadID := "t-abcdefg"
+// 	accessorUserID := "u-ZrxmQS"
 
-	if err := service.AddModerator(context.Background(), p, threadID, accessorUserID); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Successfully add moderator with user username %s for a thread with thread ID %s", p.Username, threadID)
-	}
-}
+// 	if err := service.AddModerator(context.Background(), p, threadID, accessorUserID); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Successfully add moderator with user username %s for a thread with thread ID %s", p.Username, threadID)
+// 	}
+// }
 
-func TestRemoveModerator(t *testing.T) {
-	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
+// func TestRemoveModerator(t *testing.T) {
+// 	var service ThreadService = NewThreadServiceImpl(threadRepo, categoryRepo, userRepo, idGenerator)
 
-	p := payload.AddRemoveModerator{
-		Username: "rezana",
-	}
+// 	p := payload.AddRemoveModerator{
+// 		Username: "rezana",
+// 	}
 
-	threadID := "t-abcdefg"
-	accessorUserID := "u-ZrxmQS"
+// 	threadID := "t-abcdefg"
+// 	accessorUserID := "u-ZrxmQS"
 
-	if err := service.RemoveModerator(context.Background(), p, threadID, accessorUserID); err != nil {
-		t.Fatalf("Error happened: %s", err)
-	} else {
-		t.Logf("Successfully remove moderator with user username %s for a thread with thread ID %s", p.Username, threadID)
-	}
-}
+// 	if err := service.RemoveModerator(context.Background(), p, threadID, accessorUserID); err != nil {
+// 		t.Fatalf("Error happened: %s", err)
+// 	} else {
+// 		t.Logf("Successfully remove moderator with user username %s for a thread with thread ID %s", p.Username, threadID)
+// 	}
+// }
