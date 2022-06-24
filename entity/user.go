d6ebf1e2f81e4670c2dc4d2f8a16d574cf5ec74b
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-24 16:33:37 +0700

    Revert "Add TotalFollowing field"
    This reverts commit 918a860e8e191bae14f88d8787d5ef157153bb49.

--- entity/user.go
+++ entity/user.go
@@ -3,19 +3,18 @@ package entity
 import "time"
 
 type User struct {
-	ID             string
-	Username       string
-	Email          string
-	Name           string
-	Password       string
-	Role           string
-	IsActive       bool
-	TotalThread    uint64
-	TotalFollower  uint64
-	TotalFollowing uint64
-	IsFollowed     bool
-	CreatedAt      time.Time
-	UpdatedAt      time.Time
+	ID            string
+	Username      string
+	Email         string
+	Name          string
+	Password      string
+	Role          string
+	IsActive      bool
+	TotalThread   uint64
+	TotalFollower uint64
+	IsFollowed    bool
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
 }
 
 type UserStatus bool
