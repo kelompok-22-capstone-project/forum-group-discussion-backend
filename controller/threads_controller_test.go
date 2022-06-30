cd10a3ac6738d9c60022c03d3ecac88290e67c02
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2022-06-30 16:25:55 +0700

    Create the unit testing for putUpdateThread method

--- controller/threads_controller_test.go
+++ controller/threads_controller_test.go
@@ -528,3 +528,138 @@ func TestGetThread(t *testing.T) {
 		})
 	}
 }
+
+func TestPutUpdateThread(t *testing.T) {
+	mockThreadService := &mts.ThreadService{}
+	mockTokenGenerator := &mtg.TokenGenerator{}
+
+	t.Run("success scenarion", func(t *testing.T) {
+		dummyReq := payload.UpdateThread{
+			Title:       "Go Programming Going Hype",
+			CategoryID:  "c-xyz",
+			Description: "Currently Go Programming going hype because it's popularity",
+		}
+
+		mockTokenGenerator.On(
+			"ExtractToken",
+			mock.AnythingOfType("*echo.context"),
+		).Return(
+			func(c echo.Context) generator.TokenPayload {
+				return generator.TokenPayload{
+					ID:       "u-abcdefg",
+					Username: "admin",
+					Role:     "admin",
+					IsActive: true,
+				}
+			},
+		).Once()
+
+		mockThreadService.On(
+			"Update",
+			mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+			mock.AnythingOfType(fmt.Sprintf("%T", "")),
+			mock.AnythingOfType(fmt.Sprintf("%T", "")),
+			mock.AnythingOfType(fmt.Sprintf("%T", payload.UpdateThread{})),
+		).Return(
+			func(ctx context.Context, accessorUserID string, id string, p payload.UpdateThread) error {
+				return nil
+			},
+		).Once()
+
+		t.Run("it should return 204 status code, when there is no error", func(t *testing.T) {
+			controller := NewThreadsController(mockThreadService, mockTokenGenerator)
+			requestBody, err := json.Marshal(dummyReq)
+			assert.NoError(t, err)
+
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodPut, "/api/v1/categories", strings.NewReader(string(requestBody)))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetPath("/:id")
+			c.SetParamNames("id")
+			c.SetParamValues("c-xyz")
+
+			if assert.NoError(t, controller.putUpdateThread(c)) {
+				assert.Equal(t, http.StatusNoContent, rec.Code)
+			}
+		})
+	})
+
+	t.Run("failed scenario", func(t *testing.T) {
+		dummyReq := payload.UpdateThread{
+			Title:       "Go Programming Going Hype",
+			CategoryID:  "c-xyz",
+			Description: "Currently Go Programming going hype because it's popularity",
+		}
+
+		testCases := []struct {
+			name                 string
+			inputPayload         payload.UpdateThread
+			expectedStatusCode   int
+			expectedErrorMessage string
+			mockBehaviours       func()
+		}{
+			{
+				name:                 "it should return 500 status code, when error happened",
+				inputPayload:         dummyReq,
+				expectedStatusCode:   http.StatusInternalServerError,
+				expectedErrorMessage: "Something went wrong.",
+				mockBehaviours: func() {
+					mockTokenGenerator.On(
+						"ExtractToken",
+						mock.AnythingOfType("*echo.context"),
+					).Return(
+						func(c echo.Context) generator.TokenPayload {
+							return generator.TokenPayload{
+								ID:       "u-abcdefg",
+								Username: "admin",
+								Role:     "admin",
+								IsActive: true,
+							}
+						},
+					).Once()
+
+					mockThreadService.On(
+						"Update",
+						mock.AnythingOfType(fmt.Sprintf("%T", context.Background())),
+						mock.AnythingOfType(fmt.Sprintf("%T", "")),
+						mock.AnythingOfType(fmt.Sprintf("%T", "")),
+						mock.AnythingOfType(fmt.Sprintf("%T", payload.UpdateThread{})),
+					).Return(
+						func(ctx context.Context, accessorUserID string, id string, p payload.UpdateThread) error {
+							return service.ErrRepository
+						},
+					).Once()
+				},
+			},
+		}
+
+		for _, testCase := range testCases {
+			t.Run(testCase.name, func(t *testing.T) {
+				testCase.mockBehaviours()
+
+				controller := NewThreadsController(mockThreadService, mockTokenGenerator)
+				requestBody, err := json.Marshal(dummyReq)
+				assert.NoError(t, err)
+
+				e := echo.New()
+				req := httptest.NewRequest(http.MethodPut, "/api/v1/threads", strings.NewReader(string(requestBody)))
+				req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+				rec := httptest.NewRecorder()
+				c := e.NewContext(req, rec)
+				c.SetPath("/:id")
+				c.SetParamNames("id")
+				c.SetParamValues("c-xyz")
+
+				gotErr := controller.putUpdateThread(c)
+				if assert.Error(t, gotErr) {
+					if echoHTTPError, ok := gotErr.(*echo.HTTPError); assert.Equal(t, true, ok) {
+						assert.Equal(t, testCase.expectedStatusCode, echoHTTPError.Code)
+						assert.Equal(t, testCase.expectedErrorMessage, echoHTTPError.Message)
+					}
+				}
+			})
+		}
+	})
+}
